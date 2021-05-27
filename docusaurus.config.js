222cb6966bbedb4f8e6fbd55f10e141f7fb00a3e
Alex Baker <alex@tasks.org>
2021-05-27 10:00:15 -0500

    Fix edit links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -101,13 +101,13 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+            'https://github.com/tasks/docs/edit/master/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+            'https://github.com/tasks/docs/edit/master/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
